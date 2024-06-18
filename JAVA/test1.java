5394a04877cf4fbf72fcad164bf819239fae169c
Meo-0 <dlaeodud12@gmail.com>
2024-06-18 15:50:59 +0900

    Add files via upload

---
+++ JAVA/test1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args)
+    {
+        System.out.println("hellow world!");
+        String text = "나는 바보입니다.";
+        System.out.println(text);
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int input = keyboard.nextInt();
+
+        System.out.println(input + 3);
+
+    }
+}
